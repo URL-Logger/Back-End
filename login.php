59c0987dc051e3248f9982f7f265ce52c319a0af
Jarad Shelton <jarad.shelton@aruio.net>
2017-04-26 15:24:39 -0700

    Fixed dependency targets for login scripts.

--- login.php
+++ login.php
@@ -1,30 +1,23 @@
 <?php
 require_once("src/lib/db.php");
-require_once("src/lib/secure.php");
+require_once("src/scripts/secure.php");
 require_once("src/misc/database.php");
 
 $user = (isset($_POST['user']))? $_POST['user'] : null;
 $pass = (isset($_POST['pass']))? $_POST['pass'] : null;
 
 $db = DB::connect($_DB['HOST'], $_DB['READ_USER_LOGIN']['USER'], $_DB['READ_USER_LOGIN']['PASS'], $_DB['DATABASE']);
-
-if($db === null)
-	die("failed");
+if($db === null) die("failed");
 
 if( !$db->prepare("getUser", "SELECT ID, Email, Password FROM `User_Login` WHERE Email=? LIMIT 1") )
 	die($db->error());
 $db->param("getUser", "s", $user);
 $result = $db->execute("getUser");
 
-if($user && $pass) {
+if($user !== null && $pass !== null) {
 	if($result !== null) {
-		if(encrypt_password($user, $pass) == $result[0]['Password'])
+		if($pass == $result[0]['Password'])
 			echo $result[0]['ID'];
-		else
-			echo "INVALID_PASS";
-	}
-	else
-		echo "INVALID_USER";
+		else echo 0;
+	} else echo 0;
 }
-else
-	echo "BAD_PARAMS";
--- src/login.php
+++ src/login.php
@@ -1,52 +1,45 @@
 <?php
-$out = "";
 if(isset($_POST['login'])) {
-	$user = (!empty($_POST['user']))? $_POST['user'] : null;
-	$pass = (!empty($_POST['pass']))? $_POST['pass'] : null;
-	if($user !== null && $pass !== null) {
-		$db = DB::connect($_DB['HOST'], $_DB['READ_ADMIN_LOGIN']['USER'], $_DB['READ_ADMIN_LOGIN']['PASS'], $_DB['DATABASE']);
-		if(!$db->prepare("GetUserLogin", "SELECT ID, Email, Password, Secure FROM `Admin_Login` WHERE Email=? LIMIT 1"))
-			die($db->error());
-		$db->param("GetUserLogin", "s", $user);
-		$result = $db->execute("GetUserLogin");
-		if($result !== null) {
-			if($result[0]['Secure'] !== null) {
-				$dbs = DB::connect($_DB['HOST'], $_DB['READ_SECURITY_LOGIN']['USER'], $_DB['READ_SECURITY_LOGIN']['PASS'], $_DB['DATABASE']);
-				$dbs->prepare("getSalt", "SELECT Salt FROM `Security_Salt` WHERE ID=?");
-				$dbs->param("getSalt", "i", $result[0]['Secure']);
-				$res_salt = $dbs->execute("getSalt");
-				if($res_salt) {
-					if(password_verify($pass. $res_salt[0]['Salt'], $result[0]['Password'])) {
-						$_SESSION['ADMIN_USER'] = $result[0]['ID'];
-						header("Refresh: 0");
-						exit;
-					}
-					else $out = "Invalid login credentials.</br>";
-				}
-				else $out = "An internal error has occurred.</br>";
-			}
-			else {
-				if($pass == $result[0]['Password']) {
+	$user = (!empty($_POST['user']))? $_POST['user'] : "";
+	$pass = (!empty($_POST['pass']))? $_POST['pass'] : "";
+	$db = DB::connect($_DB['HOST'], $_DB['READ_ADMIN_LOGIN']['USER'], $_DB['READ_ADMIN_LOGIN']['PASS'], $_DB['DATABASE']);
+	if(!$db->prepare("GetUserLogin", "SELECT ID, Email, Password, Secure FROM `Admin_Login` WHERE Email=? LIMIT 1"))
+		die($db->error());
+	$db->param("GetUserLogin", "s", $user);
+	$result = $db->execute("GetUserLogin");
+	if($result !== null) {
+		if($result[0]['Secure'] !== null) {
+			$dbs = DB::connect($_DB['HOST'], $_DB['READ_SECURITY_LOGIN']['USER'], $_DB['READ_SECURITY_LOGIN']['PASS'], $_DB['DATABASE']);
+			$dbs->prepare("getSalt", "SELECT Salt FROM `Security_Salt` WHERE ID=?");
+			$dbs->param("getSalt", "i", $result[0]['Secure']);
+			$res_salt = $dbs->execute("getSalt");
+			if($res_salt) {
+				if(password_verify($pass. $res_salt[0]['Salt'], $result[0]['Password'])) {
 					$_SESSION['ADMIN_USER'] = $result[0]['ID'];
 					header("Refresh: 0");
 					exit;
-				}
-				else $out = "Invalid login credentials.";
-			}
+				} else $out = "Invalid login credentials.";
+			} else $out = "An internal error has occurred.";
 		}
-		else $out = "Invalid login credentials.";
-	}
+		else {
+			if($pass == $result[0]['Password']) {
+				$_SESSION['ADMIN_USER'] = $result[0]['ID'];
+				header("Refresh: 0");
+				exit;
+			} else $out = "Invalid login credentials.";
+		}
+	} else $out = "Invalid login credentials.";
 }
 ?>
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title></title>
+		<title>Utelem - Login</title>
 		<style></style>
 		<script></script>
 	</head>
 	<body>
-		<?php if($out !== null) print $out; ?></br>
+		<?php if(isset($out)) print $out; else print "Login"; ?></br>
 		<form method="POST">
 			<input type="text" name="user" placeholder="Email"/></br>
 			<input type="password" name="pass" placeholder="Password"/></br>
